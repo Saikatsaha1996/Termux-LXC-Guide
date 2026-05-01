4640f4b49de26cda93e702826f0670ade979d278
SS <72664192+Saikatsaha1996@users.noreply.github.com>
2026-05-02 01:21:35 +0530

    Update utils.post-stop.sh

--- src/required-lxc-configuration/scripts/utils/utils.post-stop.sh
+++ src/required-lxc-configuration/scripts/utils/utils.post-stop.sh
@@ -3,6 +3,7 @@
 # Post-stop script for LXC containers
 
 # If container stopped then umount the bind mounted rootfs and restore it's nosuid if it was set
-umount -Rl "${LXC_ROOTFS_PATH}"
+umount -Rl "${LXC_ROOTFS_PATH}" 2>/dev/null
+umount -l "${LXC_ROOTFS_PATH}" 2>/dev/null
 
 exit 0
